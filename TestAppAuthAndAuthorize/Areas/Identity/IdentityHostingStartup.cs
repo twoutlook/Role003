518b0615888e84a5a31f0df367a22ecf54520348
Mark Chenpingling <chenpingling@gmail.com>
2020-10-24 22:17:30 +0800

    INIT

---
+++ TestAppAuthAndAuthorize/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestAppAuthAndAuthorize.Data;
+
+[assembly: HostingStartup(typeof(TestAppAuthAndAuthorize.Areas.Identity.IdentityHostingStartup))]
+namespace TestAppAuthAndAuthorize.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TestAppAuthAndAuthorizeContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TestAppAuthAndAuthorizeContextConnection")));
+            //https://stackoverflow.com/questions/51161729/addidentity-fails-invalidoperationexception-scheme-already-exists-identity
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<TestAppAuthAndAuthorizeContext>();
+            });
+        }
+    }
+}
